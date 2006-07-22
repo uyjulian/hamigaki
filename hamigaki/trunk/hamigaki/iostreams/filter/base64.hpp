734d66da892765e9c2a25e63a37c4fbac9189599
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-07-22 09:08:34 +0000

    add urlsafe version Base64 filter
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@14 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/iostreams/filter/base64.hpp
+++ hamigaki/trunk/hamigaki/iostreams/filter/base64.hpp
@@ -17,108 +17,24 @@
 
 namespace hamigaki { namespace iostreams {
 
-class base64_encoder
-    : public arbitrary_positional_facade<base64_encoder, char, 3>
+struct base64_traits
 {
-    friend class core_access;
+    static const char padding = '=';
 
-    typedef boost::uint_t<24>::fast uint24_t;
-
-public:
-    typedef char char_type;
-
-    struct category
-        : public boost::iostreams::output
-        , public boost::iostreams::filter_tag
-        , public boost::iostreams::multichar_tag
-        , public boost::iostreams::closable_tag
-    {};
-
-private:
-    static uint24_t char_to_uint24(char c)
-    {
-        return static_cast<uint24_t>(static_cast<unsigned char>(c) & 0xFF);
-    }
-
-    static void encode(char* dst, const char* src)
+    static char encode(unsigned char n)
     {
         const char table[] =
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
             "abcdefghijklmnopqrstuvwxyz"
             "0123456789+/";
 
-        uint24_t tmp =
-            (char_to_uint24(src[0]) << 16) |
-            (char_to_uint24(src[1]) <<  8) |
-            (char_to_uint24(src[2])      ) ;
-
-        dst[0] = table[(tmp >> 18) & 0x3F];
-        dst[1] = table[(tmp >> 12) & 0x3F];
-        dst[2] = table[(tmp >>  6) & 0x3F];
-        dst[3] = table[(tmp      ) & 0x3F];
+        return table[n];
     }
 
-    template<class Sink>
-    std::streamsize write_blocks(Sink& sink, const char* s, std::streamsize n)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            char buf[4];
-            encode(buf, s);
-            boost::iostreams::write(sink, buf, sizeof(buf));
-            s += 3;
-        }
-        return n*3;
-    }
-
-    template<class Sink>
-    void close_with_flush(Sink& sink, const char* s, std::streamsize n)
-    {
-        if (n != 0)
-        {
-            char src[3] = { 0, 0, 0 };
-            std::copy(s, s+n, &src[0]);
-
-            char buf[4];
-            encode(buf, src);
-
-            if (n == 1)
-                buf[2] = '=';
-            buf[3] = '=';
-
-            boost::iostreams::write(sink, buf, sizeof(buf));
-        }
-    }
-};
-
-class base64_decoder
-    : public arbitrary_positional_facade<base64_decoder, char, 3>
-{
-    friend class core_access;
-
-    typedef boost::uint_t<24>::fast uint24_t;
-    typedef boost::uint_t<6>::least uint6_t;
-
-public:
-    typedef char char_type;
-
-    struct category
-        : public boost::iostreams::input
-        , public boost::iostreams::filter_tag
-        , public boost::iostreams::multichar_tag
-        , public boost::iostreams::closable_tag
-    {};
-
-private:
-    static char uint24_to_char(uint24_t n)
-    {
-        return static_cast<char>(static_cast<unsigned char>(n & 0xFF));
-    }
-
-    static uint24_t decode(char c)
+    static unsigned char decode(char c)
     {
         // TODO: should support non-ASCII
-        const uint6_t table[] =
+        const unsigned char table[] =
         {
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
@@ -147,10 +63,164 @@ private:
         unsigned char uc = static_cast<unsigned char>(c);
         if (static_cast<std::size_t>(uc) >= sizeof(table))
             throw BOOST_IOSTREAMS_FAILURE("bad Base64 sequence");
-        uint6_t val = table[uc];
+        unsigned char val = table[uc];
         if (val == 0xFF)
             throw BOOST_IOSTREAMS_FAILURE("bad Base64 sequence");
         return val;
+    }
+};
+
+struct urlsafe_base64_traits
+{
+    static const char padding = '=';
+
+    static char encode(unsigned char n)
+    {
+        const char table[] =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz"
+            "0123456789-_";
+
+        return table[n];
+    }
+
+    static unsigned char decode(char c)
+    {
+        // TODO: should support non-ASCII
+        const unsigned char table[] =
+        {
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x3E, 0xFF, 0xFF,
+
+            0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B,
+            0x3C, 0x3D, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+
+            0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06,
+            0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
+
+            0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+            0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0x3F,
+
+            0xFF, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20,
+            0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28,
+
+            0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30,
+            0x31, 0x32, 0x33, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+        };
+        unsigned char uc = static_cast<unsigned char>(c);
+        if (static_cast<std::size_t>(uc) >= sizeof(table))
+            throw BOOST_IOSTREAMS_FAILURE("bad URL-safe Base64 sequence");
+        unsigned char val = table[uc];
+        if (val == 0xFF)
+            throw BOOST_IOSTREAMS_FAILURE("bad URL-safe Base64 sequence");
+        return val;
+    }
+};
+
+template<class Traits>
+class basic_base64_encoder
+    : public arbitrary_positional_facade<basic_base64_encoder<Traits>, char, 3>
+{
+    friend class core_access;
+
+    typedef boost::uint_t<24>::fast uint24_t;
+
+public:
+    typedef char char_type;
+
+    struct category
+        : public boost::iostreams::output
+        , public boost::iostreams::filter_tag
+        , public boost::iostreams::multichar_tag
+        , public boost::iostreams::closable_tag
+    {};
+
+private:
+    static uint24_t char_to_uint24(char c)
+    {
+        return static_cast<uint24_t>(static_cast<unsigned char>(c) & 0xFF);
+    }
+
+    static void encode(char* dst, const char* src)
+    {
+        uint24_t tmp =
+            (char_to_uint24(src[0]) << 16) |
+            (char_to_uint24(src[1]) <<  8) |
+            (char_to_uint24(src[2])      ) ;
+
+        dst[0] = Traits::encode((tmp >> 18) & 0x3F);
+        dst[1] = Traits::encode((tmp >> 12) & 0x3F);
+        dst[2] = Traits::encode((tmp >>  6) & 0x3F);
+        dst[3] = Traits::encode((tmp      ) & 0x3F);
+    }
+
+    template<class Sink>
+    std::streamsize write_blocks(Sink& sink, const char* s, std::streamsize n)
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            char buf[4];
+            encode(buf, s);
+            boost::iostreams::write(sink, buf, sizeof(buf));
+            s += 3;
+        }
+        return n*3;
+    }
+
+    template<class Sink>
+    void close_with_flush(Sink& sink, const char* s, std::streamsize n)
+    {
+        if (n != 0)
+        {
+            char src[3] = { 0, 0, 0 };
+            std::copy(s, s+n, &src[0]);
+
+            char buf[4];
+            encode(buf, src);
+
+            if (n == 1)
+                buf[2] = Traits::padding;
+            buf[3] = Traits::padding;
+
+            boost::iostreams::write(sink, buf, sizeof(buf));
+        }
+    }
+};
+
+template<class Traits>
+class basic_base64_decoder
+    : public arbitrary_positional_facade<basic_base64_decoder<Traits>, char, 3>
+{
+    friend class core_access;
+
+    typedef boost::uint_t<24>::fast uint24_t;
+    typedef boost::uint_t<6>::least uint6_t;
+
+public:
+    typedef char char_type;
+
+    struct category
+        : public boost::iostreams::input
+        , public boost::iostreams::filter_tag
+        , public boost::iostreams::multichar_tag
+        , public boost::iostreams::closable_tag
+    {};
+
+private:
+    static char uint24_to_char(uint24_t n)
+    {
+        return static_cast<char>(static_cast<unsigned char>(n & 0xFF));
+    }
+
+    static uint24_t decode(char c)
+    {
+        return Traits::decode(c);
     }
 
     static std::streamsize decode(char* dst, const char* src)
@@ -158,11 +228,11 @@ private:
         char buf[4];
         std::copy(src, src+4, buf);
 
-        if (src[3] == '=')
+        if (src[3] == Traits::padding)
         {
-            buf[3] = 'A';
-            if (src[2] == '=')
-                buf[2] = 'A';
+            buf[3] = Traits::encode(0);
+            if (src[2] == Traits::padding)
+                buf[2] = Traits::encode(0);
         }
 
         uint24_t tmp =
@@ -173,9 +243,9 @@ private:
 
         std::streamsize n = 0;
         dst[n++] = uint24_to_char(tmp >> 16);
-        if (src[2] != '=')
+        if (src[2] != Traits::padding)
             dst[n++] = uint24_to_char(tmp >>  8);
-        if (src[3] != '=')
+        if (src[3] != Traits::padding)
             dst[n++] = uint24_to_char(tmp      );
         return n;
     }
@@ -193,7 +263,7 @@ private:
                 break;
             std::streamsize amt = decode(s, buf);
             s += amt;
-            total += amt;;
+            total += amt;
         }
         return (total != 0) ? total : -1;
     }
@@ -203,6 +273,12 @@ private:
     {
     }
 };
+
+typedef basic_base64_encoder<base64_traits> base64_encoder;
+typedef basic_base64_decoder<base64_traits> base64_decoder;
+
+typedef basic_base64_encoder<urlsafe_base64_traits> urlsafe_base64_encoder;
+typedef basic_base64_decoder<urlsafe_base64_traits> urlsafe_base64_decoder;
 
 } } // End namespaces iostreams, hamigaki.
 
--- hamigaki/trunk/libs/iostreams/test/urlsafe_base64_test.cpp
+++ hamigaki/trunk/libs/iostreams/test/urlsafe_base64_test.cpp
@@ -1,4 +1,4 @@
-//  base64_test.cpp: test case for Base64 encoder/decoder
+//  urlsafe_base64_test.cpp: test case for URL-safe Base64 encoder/decoder
 
 //  Copyright Takeshi Mouri 2006.
 //  Use, modification, and distribution are subject to the
@@ -23,7 +23,7 @@ std::string base64_encode(const std::string& src)
     std::string dst;
     io::copy(
         io::array_source(start, start + src.size()),
-        io::compose(io_ex::base64_encoder(), io::back_inserter(dst))
+        io::compose(io_ex::urlsafe_base64_encoder(), io::back_inserter(dst))
     );
     return dst;
 }
@@ -35,7 +35,7 @@ std::string base64_decode(const std::string& src)
     std::string dst;
     io::copy(
         io::compose(
-            io_ex::base64_decoder(),
+            io_ex::urlsafe_base64_decoder(),
             io::array_source(start, start + src.size())
         ),
         io::back_inserter(dst)
@@ -43,21 +43,21 @@ std::string base64_decode(const std::string& src)
     return dst;
 }
 
-void base64_test()
+void urlsafe_base64_test()
 {
     BOOST_CHECK_EQUAL(
-        base64_encode("\x14\xfb\x9c\x03\xd9\x7f"), std::string("FPucA9l/"));
+        base64_encode("\x14\xfb\x9c\x03\xd9\x7f"), std::string("FPucA9l_"));
     BOOST_CHECK_EQUAL(
-        base64_encode("\x14\xfb\x9c\x03\xd9\x7e"), std::string("FPucA9l+"));
+        base64_encode("\x14\xfb\x9c\x03\xd9\x7e"), std::string("FPucA9l-"));
     BOOST_CHECK_EQUAL(
         base64_encode("\x14\xfb\x9c\x03\xd9"), std::string("FPucA9k="));
     BOOST_CHECK_EQUAL(
         base64_encode("\x14\xfb\x9c\x03"), std::string("FPucAw=="));
 
     BOOST_CHECK_EQUAL(
-        base64_decode("FPucA9l/"), std::string("\x14\xfb\x9c\x03\xd9\x7f"));
+        base64_decode("FPucA9l_"), std::string("\x14\xfb\x9c\x03\xd9\x7f"));
     BOOST_CHECK_EQUAL(
-        base64_decode("FPucA9l+"), std::string("\x14\xfb\x9c\x03\xd9\x7e"));
+        base64_decode("FPucA9l-"), std::string("\x14\xfb\x9c\x03\xd9\x7e"));
     BOOST_CHECK_EQUAL(
         base64_decode("FPucA9k="), std::string("\x14\xfb\x9c\x03\xd9"));
     BOOST_CHECK_EQUAL(
@@ -66,7 +66,7 @@ void base64_test()
 
 ut::test_suite* init_unit_test_suite(int, char* [])
 {
-    ut::test_suite* test = BOOST_TEST_SUITE("Base64 test");
-    test->add(BOOST_TEST_CASE(&base64_test));
+    ut::test_suite* test = BOOST_TEST_SUITE("URL-safe Base64 test");
+    test->add(BOOST_TEST_CASE(&urlsafe_base64_test));
     return test;
 }
