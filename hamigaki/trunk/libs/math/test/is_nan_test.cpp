c70153c36511cd368c2c173d97d5275f7d5d1c31
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-08-13 13:50:48 +0000

    workaround for Borland 5.82
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@101 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/libs/math/test/is_nan_test.cpp
+++ hamigaki/trunk/libs/math/test/is_nan_test.cpp
@@ -9,8 +9,25 @@
 
 #include <hamigaki/math/is_nan.hpp>
 #include <boost/test/unit_test.hpp>
+#include <iostream>
 #include <string>
 #include <typeinfo>
+
+#if defined(_M_IX86) && \
+    BOOST_WORKAROUND(__BORLANDC__, >= 0x580) && \
+    BOOST_WORKAROUND(__BORLANDC__, <= 0x582)
+std::_Dconst _FInf  = {{ 0x0000, 0x7F80 }};
+std::_Dconst _FNan  = {{ 0x0000, 0x7FC0 }};
+std::_Dconst _FSnan = {{ 0x0001, 0x7F80 }};
+
+std::_Dconst _Inf   = {{ 0x0000, 0x0000, 0x0000, 0x7FF0 }};
+std::_Dconst _Nan   = {{ 0x0000, 0x0000, 0x0000, 0x7FF8 }};
+std::_Dconst _Snan  = {{ 0x0001, 0x0000, 0x0000, 0x7FF0 }};
+
+std::_Dconst _LInf  = {{ 0x0000, 0x0000, 0x0000, 0x8000, 0x7FFF }};
+std::_Dconst _LNan  = {{ 0x0000, 0x0000, 0x0000, 0xC000, 0x7FFF }};
+std::_Dconst _LSnan = {{ 0x0001, 0x0000, 0x0000, 0x8000, 0x7FFF }};
+#endif
 
 namespace math_ex = hamigaki::math;
 namespace ut = boost::unit_test;
@@ -18,51 +35,41 @@ namespace ut = boost::unit_test;
 template<typename T>
 void is_nan_test_aux()
 {
+    std::cout << "Testing for " << typeid(T).name() << std::endl;
+
     typedef std::numeric_limits<T> traits;
 
-    BOOST_CHECK_MESSAGE(!math_ex::is_nan(T()), typeid(T).name());
+    BOOST_CHECK(!math_ex::is_nan(T()));
 
     if (traits::is_specialized)
-    {
-        BOOST_CHECK_MESSAGE(
-            !math_ex::is_nan((traits::max)()),
-            typeid(T).name()
-        );
-    }
+        BOOST_CHECK(!math_ex::is_nan((traits::max)()));
 
-    if (traits::has_infinity )
-    {
-        BOOST_CHECK_MESSAGE(
-            !math_ex::is_nan(traits::infinity ()),
-            typeid(T).name()
-        );
-    }
+    if (traits::has_infinity)
+        BOOST_CHECK(!math_ex::is_nan(traits::infinity()));
 
     if (traits::has_quiet_NaN)
-    {
-        BOOST_CHECK_MESSAGE(
-            math_ex::is_nan(traits::quiet_NaN()),
-            typeid(T).name()
-        );
-    }
+        BOOST_CHECK(math_ex::is_nan(traits::quiet_NaN()));
 
     if (traits::has_signaling_NaN)
-    {
-        BOOST_CHECK_MESSAGE(
-            math_ex::is_nan(traits::signaling_NaN()),
-            typeid(T).name()
-        );
-    }
+        BOOST_CHECK(math_ex::is_nan(traits::signaling_NaN()));
 }
 
 void is_nan_test()
 {
+#if defined(__BORLANDC__)
+     ::_control87(EM_INVALID,MCW_EM);
+#endif
+
     is_nan_test_aux<float>();
     is_nan_test_aux<double>();
     is_nan_test_aux<long double>();
 
     is_nan_test_aux<int>();
     is_nan_test_aux<std::string>();
+
+#if defined(__BORLANDC__)
+     ::_clear87();
+#endif
 }
 
 ut::test_suite* init_unit_test_suite(int, char* [])
