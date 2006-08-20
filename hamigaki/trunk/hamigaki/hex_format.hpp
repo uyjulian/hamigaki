5cbac71304e224a4dee3e5391f078f6c3df106de
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-08-20 14:59:23 +0000

    add Hamigaki.Uuid
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@113 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/hamigaki/hex_format.hpp
@@ -0,0 +1,146 @@
+//  hex_format.hpp: hexadecimal formatting
+
+//  Copyright Takeshi Mouri 2006.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef HAMIGAKI_HEX_FORMAT_HPP
+#define HAMIGAKI_HEX_FORMAT_HPP
+
+#include <boost/assert.hpp>
+#include <boost/cstdint.hpp>
+#include <stdexcept>
+#include <string>
+
+namespace hamigaki {
+
+template<class CharT>
+struct hex_traits;
+
+template<>
+struct hex_traits<char>
+{
+    static char to_hex(int n, bool is_upper)
+    {
+        if (is_upper)
+            return "0123456789ABCDEF"[n];
+        else
+            return "0123456789abcdef"[n];
+    }
+
+    static int from_hex(char c)
+    {
+        if ((c >= '0') && (c <= '9'))
+            return c - '0';
+        else
+        {
+            switch (c)
+            {
+                default:
+                    throw std::runtime_error("bad hexadecimal character");
+                case 'A': case 'a':
+                    return 10;
+                case 'B': case 'b':
+                    return 11;
+                case 'C': case 'c':
+                    return 12;
+                case 'D': case 'd':
+                    return 13;
+                case 'E': case 'e':
+                    return 14;
+                case 'F': case 'f':
+                    return 15;
+            }
+        }
+    }
+};
+
+template<>
+struct hex_traits<wchar_t>
+{
+    static wchar_t to_hex(int n, bool is_upper)
+    {
+        if (is_upper)
+            return L"0123456789ABCDEF"[n];
+        else
+            return L"0123456789abcdef"[n];
+    }
+
+    static int from_hex(char c)
+    {
+        if ((c >= L'0') && (c <= L'9'))
+            return c - L'0';
+        else
+        {
+            switch (c)
+            {
+                default:
+                    throw std::runtime_error("bad hexadecimal character");
+                case L'A': case L'a':
+                    return 10;
+                case L'B': case L'b':
+                    return 11;
+                case L'C': case L'c':
+                    return 12;
+                case L'D': case L'd':
+                    return 13;
+                case L'E': case L'e':
+                    return 14;
+                case L'F': case L'f':
+                    return 15;
+            }
+        }
+    }
+};
+
+template<typename CharT>
+inline std::basic_string<CharT> to_hex(boost::uint8_t n, bool is_upper)
+{
+    std::basic_string<CharT> s;
+    s += hex_traits<CharT>::to_hex(n / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n % 16, is_upper);
+    return s;
+}
+
+template<typename CharT>
+inline std::basic_string<CharT> to_hex(
+    boost::uint8_t n1, boost::uint8_t n2, bool is_upper)
+{
+    std::basic_string<CharT> s;
+    s += hex_traits<CharT>::to_hex(n1 / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n1 % 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n2 / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n2 % 16, is_upper);
+    return s;
+}
+
+template<typename CharT>
+inline std::basic_string<CharT> to_hex(
+    boost::uint8_t n1, boost::uint8_t n2,
+    boost::uint8_t n3, boost::uint8_t n4, bool is_upper)
+{
+    std::basic_string<CharT> s;
+    s += hex_traits<CharT>::to_hex(n1 / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n1 % 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n2 / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n2 % 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n3 / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n3 % 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n4 / 16, is_upper);
+    s += hex_traits<CharT>::to_hex(n4 % 16, is_upper);
+    return s;
+}
+
+template<typename CharT>
+inline boost::uint8_t from_hex(CharT c1, CharT c2)
+{
+    return static_cast<boost::uint8_t>(
+        hex_traits<CharT>::from_hex(c1) * 16 +
+        hex_traits<CharT>::from_hex(c2)
+    );
+}
+
+} // End namespace hamigaki.
+
+#endif // HAMIGAKI_HEX_FORMAT_HPP
---
+++ hamigaki/trunk/hamigaki/uuid.hpp
@@ -0,0 +1,160 @@
+//  uuid.hpp: Universally Unique IDentifier class
+
+//  Copyright Takeshi Mouri 2006.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef HAMIGAKI_UUID_HPP
+#define HAMIGAKI_UUID_HPP
+
+#include <boost/config.hpp>
+
+#include <hamigaki/hex_format.hpp>
+#include <boost/array.hpp>
+#include <boost/cstdint.hpp>
+#include <boost/operators.hpp>
+#include <string>
+
+namespace hamigaki {
+
+class uuid : boost::totally_ordered<uuid>
+{
+public:
+    uuid()
+    {
+        data_.assign(0);
+    }
+
+    explicit uuid(const char* s)
+    {
+        std::size_t len = 36;
+        if (*s == '{')
+        {
+            if (std::strlen(s) != 38)
+                invalid_uuid();
+            ++s;
+        }
+        else if (std::strlen(s) != 36)
+            invalid_uuid();
+        from_string_impl<char,'-'>(s);
+    }
+
+#if !defined(BOOST_NO_STD_WSTRING)
+    explicit uuid(const wchar_t* s)
+    {
+        std::size_t len = 36;
+        if (*s == '{')
+        {
+            if (std::wcslen(s) != 38)
+                invalid_uuid();
+            ++s;
+        }
+        else if (std::wcslen(s) != 36)
+            invalid_uuid();
+        from_string_impl<wchar_t,'-'>(s);
+    }
+#endif
+
+    bool operator<(const uuid& rhs) const
+    {
+        return data_ < rhs.data_;
+    }
+
+    bool operator==(const uuid& rhs) const
+    {
+        return data_ == rhs.data_;
+    }
+
+    std::size_t size() const
+    {
+        return data_.size();
+    }
+
+    const boost::uint8_t* data() const
+    {
+        return data_.data();
+    }
+
+    std::string to_string() const
+    {
+        std::string s;
+        s.reserve(36);
+        this->to_string_impl<char,'-'>(s, false);
+        return s;
+    }
+
+    std::string to_guid_string() const
+    {
+        std::string s;
+        s.reserve(38);
+        s += '{';
+        this->to_string_impl<char,'-'>(s, true);
+        s += '}';
+        return s;
+    }
+
+#if !defined(BOOST_NO_STD_WSTRING)
+    std::wstring to_wstring() const
+    {
+        std::wstring s;
+        s.reserve(36);
+        this->to_string_impl<wchar_t,L'-'>(s, false);
+        return s;
+    }
+
+    std::wstring to_guid_wstring() const
+    {
+        std::wstring s;
+        s.reserve(38);
+        s += L'{';
+        this->to_string_impl<wchar_t,L'-'>(s, true);
+        s += L'}';
+        return s;
+    }
+#endif
+
+private:
+    boost::array<boost::uint8_t,16> data_;
+
+    template<class CharT, CharT Delim>
+    void from_string_impl(const CharT* s)
+    {
+        for (std::size_t i = 0; i < 16; ++i)
+        {
+            data_[i] = from_hex(s[0], s[1]);
+            s += 2;
+            if ((i == 3) || (i == 5) || (i == 7) || (i == 9))
+            {
+                if (*s != '-')
+                    invalid_uuid();
+                ++s;
+            }
+        }
+    }
+
+    template<class CharT, CharT Delim>
+    void to_string_impl(std::basic_string<CharT>& s, bool is_upper) const
+    {
+        s += hamigaki::to_hex<CharT>(
+            data_[0], data_[1], data_[2], data_[3], is_upper);
+        s += Delim;
+        s += hamigaki::to_hex<CharT>(data_[4], data_[5], is_upper);
+        s += Delim;
+        s += hamigaki::to_hex<CharT>(data_[6], data_[7], is_upper);
+        s += Delim;
+        s += hamigaki::to_hex<CharT>(data_[8], data_[9], is_upper);
+        s += Delim;
+        for (std::size_t i = 10; i < 16; ++i)
+            s += hamigaki::to_hex<CharT>(data_[i], is_upper);
+    }
+
+    static void invalid_uuid()
+    {
+        throw std::runtime_error("invalid uuid string");
+    }
+};
+
+} // End namespace hamigaki.
+
+#endif // HAMIGAKI_UUID_HPP
