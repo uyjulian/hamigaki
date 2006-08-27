80cbd62bf1527bf3a63bdd600e03acf1fdca20f3
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-08-27 07:09:17 +0000

    support RDTSC
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@131 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/detail/windows/random.hpp
+++ hamigaki/trunk/hamigaki/detail/windows/random.hpp
@@ -14,10 +14,51 @@
 
 namespace hamigaki { namespace detail { namespace windows {
 
+#if defined(_M_IX86) && defined(__BORLANDC__)
+#pragma option push -w-8070
+__declspec(naked) inline unsigned long long rdstc()
+{
+    __emit__(0x0F, 0x31, 0xC3);
+}
+#pragma option pop
+#endif
+
 inline boost::uint32_t random_seed()
 {
     std::size_t seed = 0;
+#if defined(_M_IX86) && (defined(_MSC_VER) || defined(__MWERKS__))
+    boost::uint32_t low_val;
+    boost::uint32_t high_val;
+    __asm
+    {
+        push eax
+        push edx
+        rdtsc
+        mov low_val, eax
+        mov high_val, edx
+        pop edx
+        pop eax
+    }
+    boost::hash_combine(seed, low_val);
+    boost::hash_combine(seed, high_val);
+#elif defined(_M_IX86) && defined(__BORLANDC__)
+    seed ^= hamigaki::hash_value_ui64(rdstc());
+#elif defined(__i386__) && defined(__GNUC__)
+    boost::uint32_t low;
+    boost::uint32_t high;
+    __asm__
+    (
+        "rdtsc\n\t"
+        "movl %%eax, %0\n\t"
+        "movl %%edx, %1" ::
+        "m"(low), "m"(high) :
+        "%eax", "%edx"
+    );
+    boost::hash_combine(seed, low);
+    boost::hash_combine(seed, high);
+#else
     boost::hash_combine(seed, ::GetTickCount());
+#endif
     boost::hash_combine(seed, ::GetCurrentThreadId());
     return hamigaki::hash_value_to_ui32(seed);
 }
