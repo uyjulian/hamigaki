015d9bb0b420657d2f498e369d3d04f28fb42471
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-08-29 12:52:40 +0000

    fix link comment
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@136 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/iostreams/detail/buffer.hpp
+++ hamigaki/trunk/hamigaki/iostreams/detail/buffer.hpp
@@ -5,13 +5,13 @@
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+//  See http://hamigaki.sourceforge.jp/libs/iostreams for library home page.
+
 // Original Copyright
 // ===========================================================================>
 // (C) Copyright Jonathan Turkanis 2005.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
-
-//  See http://hamigaki.sourceforge.jp/libs/iostreams for library home page.
 
 // See http://www.boost.org/libs/iostreams for documentation.
 // <===========================================================================
