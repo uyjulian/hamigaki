7334693003bc753ceb0a0de33898958869b4d60a
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-09-01 09:23:53 +0000

    not to use iterator_facade for Borland 5.82
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@143 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/coroutine/generator.hpp
+++ hamigaki/trunk/hamigaki/coroutine/generator.hpp
@@ -20,21 +20,17 @@
 #define HAMIGAKI_COROUTINE_GENERATOR_HPP
 
 #include <hamigaki/coroutine/coroutine.hpp>
-#include <boost/iterator/iterator_facade.hpp>
 #include <boost/shared_ptr.hpp>
+#include <iterator>
 
 namespace hamigaki { namespace coroutine {
 
 template<class T>
 class generator
-    : public boost::iterator_facade<
-        generator<T>,
-        const T,
-        boost::single_pass_traversal_tag
+    : public std::iterator<
+        std::input_iterator_tag, T, std::ptrdiff_t, const T, const T&
     >
 {
-    friend class boost::iterator_core_access;
-
 public:
     typedef typename coroutine<T>::self self;
 
@@ -48,24 +44,47 @@ public:
         increment();
     }
 
-private:
-    boost::shared_ptr<coroutine<T> > coro_ptr_;
+    bool operator==(const generator& rhs) const
+    {
+        return coro_ptr_ == rhs.coro_ptr_;
+    }
 
-    const T& dereference() const
+    bool operator!=(const generator& rhs) const
+    {
+        return coro_ptr_ != rhs.coro_ptr_;
+    }
+
+    const T& operator*() const
     {
         return coro_ptr_->result();
     }
+
+    const T* operator->() const
+    {
+        return &coro_ptr_->result();
+    }
+
+    generator& operator++()
+    {
+        increment();
+        return *this;
+    }
+
+    generator operator++(int)
+    {
+        generator tmp(*this);
+        increment();
+        return tmp;
+    }
+
+private:
+    boost::shared_ptr<coroutine<T> > coro_ptr_;
 
     void increment()
     {
         coro_ptr_->yield();
         if (coro_ptr_->exited())
             coro_ptr_.reset();
-    }
-
-    bool equal(const generator& rhs) const
-    {
-        return coro_ptr_ == rhs.coro_ptr_;
     }
 };
 
