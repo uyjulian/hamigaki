e5bbd43e17154a3ffeda77139b1b52c953a72c4d
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-09-09 05:03:46 +0000

    move Win32 Fiber API declaration to the separate file
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@169 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/coroutine/detail/fiber_context.hpp
+++ hamigaki/trunk/hamigaki/coroutine/detail/fiber_context.hpp
@@ -19,6 +19,7 @@
 #ifndef HAMIGAKI_COROUTINE_DETAIL_FIBER_CONTEXT_HPP
 #define HAMIGAKI_COROUTINE_DETAIL_FIBER_CONTEXT_HPP
 
+#include <hamigaki/coroutine/detail/windows/fiber.hpp>
 #include <hamigaki/coroutine/detail/swap_context_hints.hpp>
 #include <boost/assert.hpp>
 #include <boost/noncopyable.hpp>
@@ -37,70 +38,10 @@
     #include <hamigaki/coroutine/detail/gcc/sjlj_context.hpp>
 #endif
 
-#if defined(_MSC_FULL_VER) && (_MSC_FULL_VER >= 13012035)
-    #define HAMIGAKI_COROUTINE_HAS_READFSDWORD
-    extern "C" unsigned long __readfsdword(unsigned long);
-    #pragma intrinsic(__readfsdword)
-#endif
-
-extern "C" {
-
-typedef void (__stdcall* start_routine)(void*);
-
-__declspec(dllimport)
-void* __stdcall CreateFiber(unsigned long, start_routine, void*);
-
-__declspec(dllimport) void __stdcall DeleteFiber(void*);
-__declspec(dllimport) void* __stdcall ConvertThreadToFiber(void*);
-__declspec(dllimport) void __stdcall SwitchToFiber(void*);
-
-} // extern "C"
-
 namespace hamigaki { namespace coroutines { namespace detail {
 
 namespace windows
 {
-
-#if defined(HAMIGAKI_COROUTINE_HAS_READFSDWORD)
-inline void* get_current_fiber()
-{
-    return reinterpret_cast<void*>(
-        static_cast< ::uintptr_t>(::__readfsdword(0x10))
-    );
-}
-#elif (defined(_MSC_VER) || defined(__MWERKS__)) && defined(_M_IX86)
-inline void* get_current_fiber()
-{
-    __asm mov eax, fs:[0x10]
-}
-#elif defined(__BORLANDC__) && defined(_M_IX86)
-inline void* get_current_fiber()
-{
-    // mov eax, fs:[0x10]
-    __emit__(0x64, 0xA1, 0x10, 0x00, 0x00, 0x00);
-
-    return reinterpret_cast<void*>(_EAX);
-}
-#elif defined(__GNUC__) && defined(__i386__)
-inline void* get_current_fiber()
-{
-    void* result;
-    __asm__
-    (
-        "movl %%fs:0x10,%0"
-        : "=r" (result)
-    );
-    return result;
-}
-#else
-    #error unsupported platform
-#endif
-
-inline bool is_fiber()
-{
-    void* p = get_current_fiber();
-    return (p != 0) && (p != reinterpret_cast<void*>(0x00001E00));
-}
 
 class fiber_context_impl_base
 {
