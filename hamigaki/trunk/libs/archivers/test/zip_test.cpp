1945bfeb87cad079877019d4107ce1efa08ad7af
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2006-12-06 12:44:50 +0000

    add unix_test()
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@401 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/libs/archivers/test/zip_test.cpp
+++ hamigaki/trunk/libs/archivers/test/zip_test.cpp
@@ -71,9 +71,72 @@ void zip_test()
     BOOST_CHECK(!src.next_entry());
 }
 
+void unix_test()
+{
+    std::string data("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+    ar::zip::header head;
+    head.path = "unix_test.dat";
+    head.encrypted = false;
+    head.method = ar::zip::method::deflate;
+    head.update_time = std::time(0);
+    head.file_size = data.size();
+    head.attributes = ar::msdos::attributes::read_only;
+    head.permissions = 0123;
+    head.comment = "test comment";
+    head.modified_time = static_cast<std::time_t>(1);
+    head.access_time = static_cast<std::time_t>(12345);
+    head.uid = 1001;
+    head.gid = 0;
+
+    io_ex::tmp_file archive;
+    ar::basic_zip_file_sink<
+        io_ex::dont_close_device<io_ex::tmp_file>
+    > sink(io_ex::dont_close(archive));
+
+    sink.create_entry(head);
+    io_ex::blocking_write(sink, &data[0], data.size());
+    sink.close();
+    sink.close_archive();
+
+    io::seek(archive, 0, BOOST_IOS::beg);
+
+    ar::basic_zip_file_source<io_ex::tmp_file> src(archive);
+
+    BOOST_CHECK(src.next_entry());
+
+    BOOST_CHECK_EQUAL(head.path.string(), src.header().path.string());
+    BOOST_CHECK(head.link_path.empty());
+    BOOST_CHECK_EQUAL(head.encrypted, src.header().encrypted);
+    BOOST_CHECK(head.method == src.header().method);
+    BOOST_CHECK((src.header().update_time - head.update_time) >= 0);
+    BOOST_CHECK((src.header().update_time - head.update_time) <= 1);
+    BOOST_CHECK_EQUAL(head.attributes, src.header().attributes);
+    BOOST_CHECK_EQUAL(head.permissions, src.header().permissions);
+
+    BOOST_REQUIRE(src.header().modified_time);
+    BOOST_CHECK_EQUAL(*head.modified_time, *src.header().modified_time);
+    BOOST_REQUIRE(src.header().access_time);
+    BOOST_CHECK_EQUAL(*head.access_time, *src.header().access_time);
+    BOOST_REQUIRE(src.header().uid);
+    BOOST_CHECK_EQUAL(*head.uid, *src.header().uid);
+    BOOST_REQUIRE(src.header().gid);
+    BOOST_CHECK_EQUAL(*head.gid, *src.header().gid);
+
+    std::string data2;
+    io::copy(src, io::back_inserter(data2));
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+        data.begin(), data.end(), data2.begin(), data2.end()
+    );
+
+    BOOST_CHECK(!src.next_entry());
+}
+
 ut::test_suite* init_unit_test_suite(int, char* [])
 {
     ut::test_suite* test = BOOST_TEST_SUITE("ZIP test");
     test->add(BOOST_TEST_CASE(&zip_test));
+    test->add(BOOST_TEST_CASE(&unix_test));
     return test;
 }
