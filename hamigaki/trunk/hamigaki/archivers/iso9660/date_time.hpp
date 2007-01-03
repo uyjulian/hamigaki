1f41800bd33f799204dcab108b82b70976fa693b
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-01-03 15:04:27 +0000

    add iso9660 headers
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@488 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/hamigaki/archivers/iso9660/date_time.hpp
@@ -0,0 +1,130 @@
+//  date_time.hpp: ISO 9660 date and time structures
+
+//  Copyright Takeshi Mouri 2006.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://hamigaki.sourceforge.jp/libs/archivers for library home page.
+
+#ifndef HAMIGAKI_ARCHIVERS_ISO9660_DATE_TIME_HPP
+#define HAMIGAKI_ARCHIVERS_ISO9660_DATE_TIME_HPP
+
+#include <hamigaki/struct_traits.hpp>
+#include <boost/mpl/list.hpp>
+#include <boost/cstdint.hpp>
+
+namespace hamigaki { namespace archivers { namespace iso9660 {
+
+struct date_time
+{
+    char year[4];
+    char month[2];
+    char day[2];
+    char hour[2];
+    char minute[2];
+    char second[2];
+    char centisecond[2];
+    boost::int8_t timezone;
+
+    date_time()
+    {
+        std::memset(year,        '0', 4);
+        std::memset(month,       '0', 2);
+        std::memset(day,         '0', 2);
+        std::memset(hour,        '0', 2);
+        std::memset(minute,      '0', 2);
+        std::memset(second,      '0', 2);
+        std::memset(centisecond, '0', 2);
+        timezone = 0;
+    }
+
+    bool empty() const
+    {
+        return
+            (std::memcmp(year,        "0000", 4) == 0) &&
+            (std::memcmp(month,       "00",   2) == 0) &&
+            (std::memcmp(day,         "00",   2) == 0) &&
+            (std::memcmp(hour,        "00",   2) == 0) &&
+            (std::memcmp(minute,      "00",   2) == 0) &&
+            (std::memcmp(second,      "00",   2) == 0) &&
+            (std::memcmp(centisecond, "00",   2) == 0) &&
+            (timezone                            == 0) ;
+    }
+};
+
+struct binary_date_time
+{
+    boost::uint8_t year;
+    boost::uint8_t month;
+    boost::uint8_t day;
+    boost::uint8_t hour;
+    boost::uint8_t minute;
+    boost::uint8_t second;
+    boost::int8_t timezone;
+
+    binary_date_time()
+        : year(0), month(0), day(0)
+        , hour(0), minute(0), second(0), timezone(0)
+    {
+    }
+
+    bool empty() const
+    {
+        return
+            (year        == 0) &&
+            (month       == 0) &&
+            (day         == 0) &&
+            (hour        == 0) &&
+            (minute      == 0) &&
+            (second      == 0) &&
+            (timezone    == 0) ;
+    }
+};
+
+} } } // End namespaces iso9660, archivers, hamigaki.
+
+namespace hamigaki
+{
+
+template<>
+struct struct_traits<archivers::iso9660::date_time>
+{
+private:
+    typedef archivers::iso9660::date_time self;
+
+public:
+    typedef boost::mpl::list<
+        member<self, char[4], &self::year>,
+        member<self, char[2], &self::month>,
+        member<self, char[2], &self::day>,
+        member<self, char[2], &self::hour>,
+        member<self, char[2], &self::minute>,
+        member<self, char[2], &self::second>,
+        member<self, char[2], &self::centisecond>,
+        member<self, boost::int8_t, &self::timezone>
+    > members;
+};
+
+template<>
+struct struct_traits<archivers::iso9660::binary_date_time>
+{
+private:
+    typedef archivers::iso9660::binary_date_time self;
+
+public:
+    typedef boost::mpl::list<
+        member<self, boost::uint8_t, &self::year>,
+        member<self, boost::uint8_t, &self::month>,
+        member<self, boost::uint8_t, &self::day>,
+        member<self, boost::uint8_t, &self::hour>,
+        member<self, boost::uint8_t, &self::minute>,
+        member<self, boost::uint8_t, &self::second>,
+        member<self, boost::uint8_t, &self::centisecond>,
+        member<self, boost::int8_t, &self::timezone>
+    > members;
+};
+
+} // namespace hamigaki
+
+#endif // HAMIGAKI_ARCHIVERS_ISO9660_DATE_TIME_HPP
---
+++ hamigaki/trunk/hamigaki/archivers/iso9660/headers.hpp
@@ -0,0 +1,64 @@
+//  headers.hpp: ISO 9660 headers
+
+//  Copyright Takeshi Mouri 2006.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://hamigaki.sourceforge.jp/libs/archivers for library home page.
+
+#ifndef HAMIGAKI_ARCHIVERS_ISO9660_HEADERS_HPP
+#define HAMIGAKI_ARCHIVERS_ISO9660_HEADERS_HPP
+
+#include <hamigaki/archivers/iso9660/date_time.hpp>
+#include <hamigaki/archivers/iso9660/directory_record.hpp>
+#include <hamigaki/archivers/iso9660/file_flags.hpp>
+#include <hamigaki/archivers/iso9660/headers.hpp>
+#include <hamigaki/archivers/iso9660/path_table_record.hpp>
+#include <hamigaki/archivers/iso9660/volume_descriptor.hpp>
+#include <hamigaki/filesystem/consts.hpp>
+#include <boost/filesystem/path.hpp>
+#include <stdexcept>
+
+namespace hamigaki { namespace archivers { namespace iso9660 {
+
+struct header
+{
+    boost::filesystem::path path;
+    boost::uint32_t file_size;
+    binary_date_time recorded_time;
+    boost::uint8_t flags;
+
+    header() : file_size(0), flags(0)
+    {
+    }
+
+    bool is_regular() const
+    {
+        return (flags & (file_flags::directory|file_flags::associated)) == 0;
+    }
+
+    bool is_directory() const
+    {
+        return (flags & file_flags::directory) != 0;
+    }
+
+    bool is_symlink() const
+    {
+        return false;
+    }
+
+    void type(filesystem::file_type v)
+    {
+        if (v == filesystem::regular_file)
+            flags &= ~file_flags::directory;
+        else if (v == filesystem::directory_file)
+            flags |= file_flags::directory;
+        else
+            throw std::runtime_error("unsupported file type");
+    }
+};
+
+} } } // End namespaces iso9660, archivers, hamigaki.
+
+#endif // HAMIGAKI_ARCHIVERS_ISO9660_HEADERS_HPP
