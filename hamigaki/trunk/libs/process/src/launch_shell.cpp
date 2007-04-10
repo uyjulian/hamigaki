851da3c7117b435c2be210645a8965a622cdf709
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-04-10 15:23:25 +0000

    add launch_shell()
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@692 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/libs/process/src/launch_shell.cpp
@@ -0,0 +1,62 @@
+//  launch_shell.cpp: launch a process on command shell
+
+//  Copyright Takeshi Mouri 2007.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://hamigaki.sourceforge.jp/libs/process for library home page.
+
+#define HAMIGAKI_PROCESS_SOURCE
+#define NOMINMAX
+#include <boost/config.hpp>
+#include <hamigaki/process/launch_shell.hpp>
+
+#if defined(BOOST_WINDOWS)
+    #include <windows.h>
+#endif
+
+namespace hamigaki { namespace process {
+
+#if defined(BOOST_WINDOWS)
+namespace
+{
+
+std::string system_directory()
+{
+    char buf[MAX_PATH];
+    ::DWORD res = ::GetSystemDirectoryA(buf, sizeof(buf));
+    if (res == 0)
+        throw std::runtime_error("GetSystemDirectoryA() failed");
+    return std::string(buf, static_cast<std::size_t>(res));
+}
+
+} // namespace
+#endif
+
+HAMIGAKI_PROCESS_DECL
+child launch_shell(const std::string& cmd, const context& ctx)
+{
+#if defined(BOOST_WINDOWS)
+    std::string sh = system_directory();
+    if (sh[sh.size()-1] != '\\')
+        sh += '\\';
+    sh += "cmd.exe";
+
+    std::vector<std::string> args;
+    args.push_back("cmd");
+    args.push_back("/c");
+    args.push_back(cmd);
+#else // not defined(BOOST_WINDOWS)
+    std::string sh("/bin/sh");
+
+    std::vector<std::string> args;
+    args.push_back("sh");
+    args.push_back("-c");
+    args.push_back(cmd);
+#endif // not defined(BOOST_WINDOWS)
+
+    return process::child(sh, args, ctx);
+}
+
+} } // End namespaces process, hamigaki.
---
+++ hamigaki/trunk/libs/process/test/shell_test.cpp
@@ -0,0 +1,47 @@
+//  shell_test.cpp: test case for launch_shell
+
+//  Copyright Takeshi Mouri 2007.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://hamigaki.sourceforge.jp/libs/process for library home page.
+
+#include <hamigaki/process/launch_shell.hpp>
+#include <boost/iostreams/device/back_inserter.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/test/unit_test.hpp>
+
+namespace proc = hamigaki::process;
+namespace io = boost::iostreams;
+namespace ut = boost::unit_test;
+
+void sort_test()
+{
+    proc::context ctx;
+    ctx.stdin_behavior(proc::capture_stream());
+    ctx.stdout_behavior(proc::capture_stream());
+    ctx.stderr_behavior(proc::silence_stream());
+
+    proc::child c = proc::launch_shell("sort", ctx);
+
+    proc::pipe_sink& sink = c.stdin_sink();
+    sink.write("q\r\nw\r\ne\r\n", 9);
+    sink.close();
+
+    std::string dst;
+    io::copy(c.stdout_source(), io::back_inserter(dst));
+
+    BOOST_CHECK_EQUAL(dst, std::string("e\r\nq\r\nw\r\n"));
+
+    const proc::status& st = c.wait();
+    BOOST_CHECK(st.get_type() == proc::status::exited);
+    BOOST_CHECK_EQUAL(st.code(), 0u);
+}
+
+ut::test_suite* init_unit_test_suite(int, char* [])
+{
+    ut::test_suite* test = BOOST_TEST_SUITE("shell test");
+    test->add(BOOST_TEST_CASE(&sort_test));
+    return test;
+}
