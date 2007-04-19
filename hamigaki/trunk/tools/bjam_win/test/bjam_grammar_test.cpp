68f155f61b29d34177c85fae6c17a120cd9cfcd3
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-04-19 13:48:27 +0000

    add bjam_grammar_test.cpp
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@713 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/tools/bjam_win/test/bjam_grammar_test.cpp
@@ -0,0 +1,62 @@
+//  bjam_grammar_test.cpp: a test driver for bjam_grammar
+
+//  Copyright Takeshi Mouri 2007.
+//  Use, modification, and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://hamigaki.sourceforge.jp/ for library home page.
+
+#include "../impl/bjam_grammar.hpp"
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+
+using namespace boost::spirit;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 2)
+        {
+            std::cerr << "Usage: bjam_grammar_test (filename)" << std::endl;
+            return 1;
+        }
+
+        std::string str;
+        {
+            std::ifstream is(argv[1], std::ios_base::binary);
+
+            str.assign(
+                std::istreambuf_iterator<char>(is),
+                (std::istreambuf_iterator<char>())
+            );
+        }
+
+        std::vector<std::string> targets;
+        bjam_grammar g(targets);
+        bjam_skip_grammar skip;
+        parse_info<const char*> info = parse(str.c_str(), g, skip);
+
+        if (!info.full)
+        {
+            std::cerr << "Error:" << std::endl;
+            std::cerr << info.stop << std::endl;
+            return 1;
+        }
+
+        std::copy(
+            targets.begin(), targets.end(),
+            std::ostream_iterator<std::string>(std::cout, "\n")
+        );
+
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    return 1;
+}
