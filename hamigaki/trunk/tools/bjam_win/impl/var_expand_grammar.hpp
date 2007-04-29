08c29bf1962042364f51a637558ea11fb5c58b99
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-04-29 14:24:54 +0000

    implement the element selection for the variable expansion implement the modifiers "P", "U", "L" and "G" for the variable expansion
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@729 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/tools/bjam_win/impl/var_expand_grammar.hpp
+++ hamigaki/trunk/tools/bjam_win/impl/var_expand_grammar.hpp
@@ -11,9 +11,12 @@
 #define IMPL_VAR_EXPAND_GRAMMAR_HPP
 
 #include "./variables.hpp"
+#include <boost/algorithm/string/case_conv.hpp>
 #include <boost/spirit/core.hpp>
 #include <boost/spirit/attribute/closure.hpp>
 #include <boost/spirit/phoenix/binders.hpp>
+#include <boost/optional.hpp>
+#include <algorithm>
 
 struct vars_closure
     : boost::spirit::closure<
@@ -22,6 +25,30 @@ struct vars_closure
     >
 {
     member1 val;
+};
+
+struct range_closure
+    : boost::spirit::closure<
+        range_closure,
+        std::pair<unsigned,unsigned>,
+        unsigned,
+        unsigned
+    >
+{
+    member1 val;
+    member2 lower;
+    member3 upper;
+};
+
+struct expand_closure
+    : boost::spirit::closure<
+        expand_closure,
+        variables::mapped_type,
+        boost::optional<std::string>
+    >
+{
+    member1 val;
+    member2 str;
 };
 
 class vars_expand_actor
@@ -49,6 +76,125 @@ public:
 
 private:
     const variables& table_;
+};
+
+struct slice_actor
+{
+    typedef std::vector<std::string> result_type;
+
+    std::vector<std::string>
+    operator()(
+        const std::vector<std::string>& vs,
+        const std::pair<unsigned,unsigned>& r) const
+    {
+        unsigned n = r.first;
+        unsigned m = r.second;
+
+        if (m == 0)
+            m = static_cast<unsigned>(vs.size());
+
+        std::vector<std::string> result;
+        for (unsigned i = n; i <= m; ++i)
+        {
+            if (i <= static_cast<unsigned>(vs.size()))
+                result.push_back(vs[i-1]);
+            else
+                result.push_back(std::string());
+        }
+        return result;
+    }
+};
+
+struct parent_dir_actor
+{
+    typedef void result_type;
+
+    void operator()(std::vector<std::string>& vs) const
+    {
+        for (std::size_t i = 0; i < vs.size(); ++i)
+        {
+            const std::string& s = vs[i];
+
+            std::string::size_type sl = s.rfind('/');
+            std::string::size_type bs = s.rfind('\\');
+
+            const std::string::size_type npos = std::string::npos;
+            if (sl == npos)
+                sl = 0;
+            if (bs == npos)
+                bs = 0;
+
+            vs[i] = s.substr(0, (std::max)(sl, bs));
+        }
+    }
+};
+
+struct upper_actor
+{
+    typedef void result_type;
+
+    void operator()(std::vector<std::string>& vs) const
+    {
+        for (std::size_t i = 0; i < vs.size(); ++i)
+            boost::algorithm::to_upper(vs[i]);
+    }
+};
+
+struct lower_actor
+{
+    typedef void result_type;
+
+    void operator()(std::vector<std::string>& vs) const
+    {
+        for (std::size_t i = 0; i < vs.size(); ++i)
+            boost::algorithm::to_lower(vs[i]);
+    }
+};
+
+struct grist_actor
+{
+    typedef void result_type;
+
+    static std::string::size_type find_grist_end(const std::string& s)
+    {
+        if (s.empty() || (s[0] != '<'))
+            return 0;
+        else
+        {
+            std::string::size_type pos = s.find('>');
+            if (pos == std::string::npos)
+                return 0;
+            else
+                return pos+1;
+        }
+    }
+
+    void operator()(
+        std::vector<std::string>& vs,
+        const boost::optional<std::string>& val) const
+    {
+        if (val)
+        {
+            std::string grist;
+            grist += '<';
+            grist += *val;
+            grist += '>';
+
+            for (std::size_t i = 0; i < vs.size(); ++i)
+            {
+                std::string& s = vs[i];
+                s.replace(0, grist_actor::find_grist_end(s), grist);
+            }
+        }
+        else
+        {
+            for (std::size_t i = 0; i < vs.size(); ++i)
+            {
+                const std::string& s = vs[i];
+                vs[i] = s.substr(0, grist_actor::find_grist_end(s));
+            }
+        }
+    }
 };
 
 struct make_str_vec_actor
@@ -101,12 +247,39 @@ struct var_expand_grammar
         typedef boost::spirit::rule<
             ScannerT, typename vars_closure::context_t> vars_rule_t;
 
-        vars_rule_t top, expr, variable;
+        typedef boost::spirit::rule<
+            ScannerT, typename range_closure::context_t> range_rule_t;
+
+        typedef boost::spirit::rule<
+            ScannerT, typename expand_closure::context_t> expand_rule_t;
+
+        vars_rule_t top, expr;
+        range_rule_t index_range;
+        expand_rule_t variable;
 
         definition(const var_expand_grammar& self)
         {
             using namespace boost::spirit;
             using namespace phoenix;
+
+            index_range
+                =   '['
+                    >> uint_p
+                    [
+                        index_range.lower = index_range.upper = arg1
+                    ]
+                    >> !(
+                        ch_p('-')[index_range.upper = 0u]
+                        >> !uint_p[index_range.upper = arg1]
+                        )
+                    >> ch_p(']')
+                        [
+                            index_range.val =
+                                construct_<std::pair<unsigned,unsigned> >(
+                                    index_range.lower, index_range.upper
+                                )
+                        ]
+                ;
 
             variable
                 =   "$("
@@ -115,6 +288,44 @@ struct var_expand_grammar
                             variable.val =
                                 bind(vars_expand_actor(self.table))(arg1)
                         ]
+                    >> !index_range
+                        [
+                            variable.val =
+                                bind(slice_actor())(variable.val, arg1)
+                        ]
+                    >> *(
+                            ':'
+                            >> +(   ch_p('P')
+                                    [
+                                        bind(parent_dir_actor())(variable.val)
+                                    ]
+                                |   ch_p('U')
+                                    [
+                                        bind(upper_actor())(variable.val)
+                                    ]
+                                |   ch_p('L')
+                                    [
+                                        bind(lower_actor())(variable.val)
+                                    ]
+                                |   ch_p('G')
+                                    >> !(   '='
+                                            >> (*(anychar_p - ')' - ':'))
+                                            [
+                                                variable.str =
+                                                    construct_<std::string>(
+                                                        arg1, arg2
+                                                    )
+                                            ]
+                                        )
+                                    >> eps_p
+                                    [
+                                        bind(grist_actor())
+                                            (variable.val, variable.str)
+                                    ]
+                                |   alpha_p
+                                    >> !( '=' >> *(anychar_p - ')' - ':') )
+                                )
+                        )
                     >> ')'
                 ;
 
@@ -126,7 +337,7 @@ struct var_expand_grammar
                             expr.val =
                                 bind(append_str_vec_actor())(expr.val, arg1)
                         ]
-                    |   (+(anychar_p - '$' - ')'))
+                    |   (+(anychar_p - '$' - ')' - '[' - ':'))
                         [
                             expr.val =
                                 bind(append_str_vec_actor())(
--- hamigaki/trunk/tools/bjam_win/test/var_expand_grammar_test.cpp
+++ hamigaki/trunk/tools/bjam_win/test/var_expand_grammar_test.cpp
@@ -15,12 +15,32 @@
 using namespace boost::spirit;
 using namespace phoenix;
 
+void expand_test(const variables& table, const std::string& str)
+{
+    std::cout << "Input: " << str << std::endl;
+
+    var_expand_grammar g(table);
+    variables::mapped_type val;
+    parse_info<const char*> info = parse(str.c_str(), g[var(val) = arg1]);
+
+    if (info.full)
+    {
+        std::copy(
+            val.begin(), val.end(),
+            std::ostream_iterator<std::string>(std::cout, "\n")
+        );
+    }
+    else
+    {
+        std::cerr << "Error: stopping at" << std::endl;
+        std::cerr << info.stop << std::endl;
+    }
+}
+
 int main(int argc, char* argv[])
 {
     try
     {
-        std::string str = "$($(Z))";
-
         variables table;
         {
             std::vector<std::string> vs;
@@ -44,22 +64,27 @@ int main(int argc, char* argv[])
             table.add("Z");
             table.assign("Z", vs);
         }
-
-        var_expand_grammar g(table);
-        variables::mapped_type val;
-        parse_info<const char*> info = parse(str.c_str(), g[var(val) = arg1]);
-
-        if (!info.full)
         {
-            std::cerr << "Error:" << std::endl;
-            std::cerr << info.stop << std::endl;
-            return 1;
+            std::vector<std::string> vs;
+            vs.push_back("abc.txt");
+            vs.push_back("C:\\Temp\\log.txt");
+            vs.push_back("C:\\Windows\\System32\\kernel32.dll");
+            table.add("P");
+            table.assign("P", vs);
+        }
+        {
+            std::vector<std::string> vs;
+            vs.push_back("<grist>hoge");
+            vs.push_back("<non-grist");
+            vs.push_back("abc");
+            table.add("G");
+            table.assign("G", vs);
         }
 
-        std::copy(
-            val.begin(), val.end(),
-            std::ostream_iterator<std::string>(std::cout, "\n")
-        );
+        ::expand_test(table, "$($(Z)[2-4]:UL)");
+        ::expand_test(table, "$(P:P)");
+        ::expand_test(table, "$(G:G)");
+        ::expand_test(table, "$(G:G=new)");
 
         return 0;
     }
