1c1cb7cb5bbbbd59f25a94a91d78a28c49664701
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-05-24 14:03:56 +0000

    add split_path() and make_path()
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@773 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/hamigaki/bjam/util/path.hpp
@@ -0,0 +1,41 @@
+// path.hpp: path utilities
+
+// Copyright Takeshi Mouri 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://hamigaki.sourceforge.jp/libs/bjam for library home page.
+
+#ifndef HAMIGAKI_BJAM_UTIL_PATH_HPP
+#define HAMIGAKI_BJAM_UTIL_PATH_HPP
+
+#include <hamigaki/bjam/bjam_config.hpp>
+#include <string>
+
+#ifdef BOOST_HAS_ABI_HEADERS
+    #include BOOST_ABI_PREFIX
+#endif
+
+namespace hamigaki { namespace bjam {
+
+struct path_components
+{
+    std::string grist;
+    std::string root;
+    std::string dir;
+    std::string base;
+    std::string suffix;
+    std::string member;
+};
+
+HAMIGAKI_BJAM_DECL void split_path(path_components& ph, const std::string& s);
+HAMIGAKI_BJAM_DECL std::string make_path(const path_components& ph);
+
+} } // End namespaces bjam, hamigaki.
+
+#ifdef BOOST_HAS_ABI_HEADERS
+    #include BOOST_ABI_SUFFIX
+#endif
+
+#endif // HAMIGAKI_BJAM_UTIL_PATH_HPP
