468a4aad6397fd37f78e8b71d53c618efa948340
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-06-05 10:35:09 +0000

    prepare to add the class template base_definition
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@799 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/hamigaki/bjam/grammars/base_actions.hpp
@@ -0,0 +1,316 @@
+// bjam_actions.hpp: actions for bjam_grammar
+
+// Copyright Takeshi Mouri 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://hamigaki.sourceforge.jp/libs/bjam for library home page.
+
+#ifndef HAMIGAKI_BJAM_GRAMMARS_BJAM_ACTIONS_HPP
+#define HAMIGAKI_BJAM_GRAMMARS_BJAM_ACTIONS_HPP
+
+#include <hamigaki/bjam/grammars/assign_modes.hpp>
+#include <hamigaki/bjam/grammars/bjam_expression_grammar_gen.hpp>
+#include <hamigaki/bjam/grammars/bjam_grammar_gen.hpp>
+#include <hamigaki/bjam/util/variable_expansion.hpp>
+#include <hamigaki/bjam/bjam_context.hpp>
+#include <climits> // required for <boost/spirit/phoenix/operators.hpp>
+#include <boost/spirit/phoenix.hpp>
+#include <boost/next_prior.hpp>
+
+namespace hamigaki { namespace bjam {
+
+struct set_true_impl
+{
+    typedef void result_type;
+
+    void operator()(list_type& lhs, const list_type& rhs) const
+    {
+        if (!lhs)
+        {
+            if (rhs)
+                lhs = rhs;
+            else
+                lhs = list_type("1");
+        }
+    }
+
+    void operator()(list_type& lhs) const
+    {
+        if (!lhs)
+            lhs = list_type("1");
+    }
+};
+
+const ::phoenix::functor<set_true_impl> set_true = set_true_impl();
+
+
+struct try_front_impl
+{
+    typedef boost::optional<std::string> result_type;
+
+    boost::optional<std::string> operator()(const list_type& x) const
+    {
+        if (x.empty())
+            return boost::optional<std::string>();
+        else
+            return *x.begin();
+    }
+};
+
+const ::phoenix::functor<try_front_impl> try_front = try_front_impl();
+
+
+struct includes_impl
+{
+    typedef bool result_type;
+
+    bool operator()(const list_type& lhs, const list_type& rhs) const
+    {
+        typedef list_type::const_iterator iter_type;
+
+        iter_type lb = lhs.begin();
+        iter_type le = lhs.end();
+        iter_type rb = rhs.begin();
+        iter_type re = rhs.end();
+
+        for (iter_type i = lb; i != le; ++i)
+            if (std::find(rb, re, *i) == re)
+                return false;
+
+        return true;
+    }
+};
+
+const ::phoenix::functor<includes_impl> includes = includes_impl();
+
+
+struct eval_expr_impl
+{
+    typedef list_type result_type;
+
+    template<class Iterator>
+    list_type operator()(context& ctx, Iterator first, Iterator last) const
+    {
+        typedef bjam_expression_grammar_gen<Iterator> grammar_type;
+        return grammar_type::evaluate(first, last, ctx);
+    }
+};
+
+const ::phoenix::functor<eval_expr_impl> eval_expr = eval_expr_impl();
+
+
+struct var_set_impl
+{
+    typedef void result_type;
+
+    void operator()(
+        context& ctx, assign_mode::values mode,
+        const list_type& names, const list_type& values) const
+    {
+        variable_table& table =
+            ctx.current_frame().current_module().variables;
+
+        typedef list_type::const_iterator iter_type;
+
+        if (mode == assign_mode::set)
+        {
+            for (iter_type i = names.begin(), end = names.end(); i != end; ++i)
+                table.set_values(*i, values);
+        }
+        else if (mode == assign_mode::append)
+        {
+            for (iter_type i = names.begin(), end = names.end(); i != end; ++i)
+                table.append_values(*i, values);
+        }
+        else
+        {
+            for (iter_type i = names.begin(), end = names.end(); i != end; ++i)
+                table.set_default_values(*i, values);
+        }
+    }
+};
+
+const ::phoenix::functor<var_set_impl> var_set = var_set_impl();
+
+
+struct var_expand_impl
+{
+    typedef list_type result_type;
+
+    list_type operator()(context& ctx, const std::string& s) const
+    {
+        frame& f = ctx.current_frame();
+        const variable_table& table = f.current_module().variables;
+        const list_of_list& args = f.arguments();
+
+        list_type result;
+        bjam::expand_variable(result, s, table, args);
+        return result;
+    }
+};
+
+const ::phoenix::functor<var_expand_impl> var_expand = var_expand_impl();
+
+
+struct rule_set_impl
+{
+    typedef void result_type;
+
+    template<class Iterator>
+    void operator()(
+        context& ctx, const std::string& name, const list_of_list& params,
+        Iterator first, Iterator last, bool exported) const
+    {
+        frame& f = ctx.current_frame();
+        rule_table& table = f.current_module().rules;
+
+        boost::shared_ptr<rule_definition> def(new rule_definition);
+        def->parameters = params;
+        def->body.reset(new std::string(first, last));
+        def->module_name = f.module_name();
+        def->exported = exported;
+
+        table.set_rule_definition(name, def);
+    }
+};
+
+const ::phoenix::functor<rule_set_impl> rule_set = rule_set_impl();
+
+
+struct split_rule_name_impl
+{
+    typedef boost::optional<std::string> result_type;
+
+    boost::optional<std::string>
+    operator()(const list_type& values, list_of_list& args) const
+    {
+        if (values.empty())
+            return boost::optional<std::string>();
+
+        list_type arg(boost::next(values.begin()), values.end());
+        if (args.empty())
+            args.push_back(arg);
+        else
+        {
+            list_of_list tmp;
+            arg += args[0];
+            tmp.push_back(arg);
+            for (std::size_t i = 1, size = args.size(); i < size; ++i)
+                tmp.push_back(args[i]);
+            args.swap(tmp);
+        }
+
+        return values[0];
+    }
+};
+
+const ::phoenix::functor<
+    split_rule_name_impl
+> split_rule_name = split_rule_name_impl();
+
+
+struct invoke_rule_impl
+{
+    typedef list_type result_type;
+
+    list_type operator()(
+        context& ctx, const std::string& name, const list_of_list& args) const
+    {
+        return ctx.invoke_rule(name, args);
+    }
+
+    list_type operator()(
+        context& ctx,
+        const boost::optional<std::string>& name,
+        const list_of_list& args) const
+    {
+        if (name)
+            return ctx.invoke_rule(*name, args);
+        else
+            return list_type();
+    }
+};
+
+const ::phoenix::functor<invoke_rule_impl> invoke_rule = invoke_rule_impl();
+
+
+struct get_return_values_impl
+{
+    typedef list_type result_type;
+
+    list_type operator()(context& ctx) const
+    {
+        return ctx.current_frame().result();
+    }
+};
+
+const ::phoenix::functor<
+    get_return_values_impl
+> get_return_values = get_return_values_impl();
+
+
+struct set_return_values_impl
+{
+    typedef void result_type;
+
+    void operator()(context& ctx, const list_type& values) const
+    {
+        ctx.current_frame().result() = values;
+    }
+};
+
+const ::phoenix::functor<
+    set_return_values_impl
+> set_return_values = set_return_values_impl();
+
+
+struct for_block_impl
+{
+    typedef void result_type;
+
+    template<class Iterator>
+    void operator()(
+        context& ctx, const std::string& name, const list_type& values,
+        Iterator first, Iterator last, bool is_local) const
+    {
+        typedef bjam_grammar_gen<Iterator> grammar_type;
+        typedef list_type::const_iterator iter_type;
+
+        frame& f = ctx.current_frame();
+        variable_table& table = f.current_module().variables;
+
+        scoped_swap_values guard(table, name, is_local);
+        for (iter_type i = values.begin(); i != values.end(); ++i)
+        {
+            table.set_values(name, list_type(*i));
+            grammar_type::parse_bjam_grammar(first, last, ctx);
+        }
+    }
+};
+
+const ::phoenix::functor<for_block_impl> for_block = for_block_impl();
+
+
+struct while_block_impl
+{
+    typedef void result_type;
+
+    template<class Iterator>
+    void operator()(
+        context& ctx, const std::string& expr,
+        Iterator first, Iterator last) const
+    {
+        typedef bjam_grammar_gen<Iterator> grammar_type;
+
+        while (eval_expr_impl()(ctx, expr.c_str(), expr.c_str()+expr.size()))
+            grammar_type::parse_bjam_grammar(first, last, ctx);
+    }
+};
+
+const ::phoenix::functor<while_block_impl> while_block = while_block_impl();
+
+} } // End namespaces bjam, hamigaki.
+
+#endif // HAMIGAKI_BJAM_GRAMMARS_BJAM_ACTIONS_HPP
---
+++ hamigaki/trunk/hamigaki/bjam/grammars/base_closures.hpp
@@ -0,0 +1,123 @@
+// bjam_closures.hpp: closures for bjam_grammar
+
+// Copyright Takeshi Mouri 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://hamigaki.sourceforge.jp/libs/bjam for library home page.
+
+#ifndef HAMIGAKI_BJAM_GRAMMARS_BJAM_CLOSURES_HPP
+#define HAMIGAKI_BJAM_GRAMMARS_BJAM_CLOSURES_HPP
+
+#include <hamigaki/bjam/grammars/assign_modes.hpp>
+#include <hamigaki/bjam/util/list.hpp>
+#include <boost/spirit/core.hpp>
+#include <boost/spirit/attribute/closure.hpp>
+
+namespace hamigaki { namespace bjam {
+
+struct list_closure
+    : boost::spirit::closure<
+          list_closure
+        , list_type
+    >
+{
+    member1 values;
+};
+
+struct lol_closure
+    : boost::spirit::closure<
+          lol_closure
+        , list_of_list
+    >
+{
+    member1 values;
+};
+
+struct invoke_stmt_closure
+    : boost::spirit::closure<
+          invoke_stmt_closure
+        , list_type
+        , boost::optional<std::string>
+        , list_of_list
+    >
+{
+    member1 values;
+    member2 name;
+    member3 args;
+};
+
+struct assign_closure
+    : boost::spirit::closure<
+          assign_closure
+        , assign_mode::values
+    >
+{
+    member1 values;
+};
+
+struct set_stmt_closure
+    : boost::spirit::closure<
+          set_stmt_closure
+        , list_type
+        , list_type
+        , assign_mode::values
+    >
+{
+    member1 values;
+    member2 names;
+    member3 mode;
+};
+
+struct for_stmt_closure
+    : boost::spirit::closure<
+          for_stmt_closure
+        , std::string
+        , list_type
+        , bool
+    >
+{
+    member1 name;
+    member2 values;
+    member3 is_local;
+};
+
+struct module_stmt_closure
+    : boost::spirit::closure<
+          module_stmt_closure
+        , list_type
+        , boost::optional<std::string>
+    >
+{
+    member1 values;
+    member2 name;
+};
+
+struct while_stmt_closure
+    : boost::spirit::closure<
+          while_stmt_closure
+        , list_type
+        , std::string
+    >
+{
+    member1 values;
+    member2 expr;
+};
+
+struct rule_stmt_closure
+    : boost::spirit::closure<
+          rule_stmt_closure
+        , std::string
+        , list_of_list
+        , bool
+    >
+{
+    member1 name;
+    member2 params;
+    member3 exported;
+};
+
+} } // End namespaces bjam, hamigaki.
+
+#endif // HAMIGAKI_BJAM_GRAMMARS_BJAM_CLOSURES_HPP
