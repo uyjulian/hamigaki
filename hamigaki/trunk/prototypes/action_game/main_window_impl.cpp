11953fefd109ff8f4fd94bde4c5755f13f0f1512
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-08-21 10:54:40 +0000

    changed the loop of BGM to the same specification as FALCOM
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1041 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/prototypes/action_game/main_window_impl.cpp
+++ hamigaki/trunk/prototypes/action_game/main_window_impl.cpp
@@ -12,12 +12,16 @@
 #include "direct3d9.hpp"
 #include "png_loader.hpp"
 #include "sprite.hpp"
+#include <hamigaki/audio/vorbis/comment.hpp>
 #include <hamigaki/audio/background_player.hpp>
 #include <hamigaki/audio/direct_sound.hpp>
 #include <hamigaki/audio/vorbis_file.hpp>
 #include <hamigaki/audio/wide_adaptor.hpp>
 #include <hamigaki/input/direct_input.hpp>
+#include <hamigaki/iostreams/concatenate.hpp>
+#include <hamigaki/iostreams/lazy_restrict.hpp>
 #include <hamigaki/iostreams/repeat.hpp>
+#include <boost/lexical_cast.hpp>
 #include <cmath>
 #include <stdexcept>
 
@@ -51,6 +55,14 @@ input::direct_input_joystick create_joystick(::HWND handle)
     return dinput.create_joystick_device(info.instance_guid);
 }
 
+int vorbis_comment_int_value(
+    const std::pair<const char**,const char**>& comments,
+    const std::string& name)
+{
+    std::string value(audio::vorbis::comment_value(comments, name));
+    return value.empty() ? -1 : boost::lexical_cast<int>(value);
+}
+
 } // namespace
 
 class main_window::impl
@@ -58,6 +70,7 @@ class main_window::impl
 public:
     explicit impl(::HWND handle)
         : handle_(handle)
+        , vf_("bgm.ogg")
         , joystick_(create_joystick(handle_))
         , active_(false), last_time_(::GetTickCount()), frames_(0)
         , x_(32.0f), y_(416.0f), vx_(0.0f), vy_(0.0f)
@@ -147,6 +160,7 @@ public:
 private:
     ::HWND handle_;
     audio::direct_sound_device dsound_;
+    audio::vorbis_file_source vf_;
     audio::background_player bgm_;
     input::direct_input_joystick joystick_;
     direct3d9 d3d_;
@@ -163,9 +177,17 @@ private:
 
     void play_bgm()
     {
-        audio::vorbis_file_source vf("bgm.ogg");
+        const audio::vorbis_info& info = vf_.info();
+        const int block_size = info.channels;
 
-        const audio::vorbis_info& info = vf.info();
+        int loop_start =
+            vorbis_comment_int_value(vf_.comments(), "LOOPSTART");
+        std::streamsize offset = loop_start != -1 ? block_size*loop_start : -1;
+
+        int loop_length =
+            vorbis_comment_int_value(vf_.comments(), "LOOPLENGTH");
+        std::streamsize len = loop_length != -1 ? block_size*loop_length : -1;
+
         audio::pcm_format fmt;
         fmt.type = audio::int_le16;
         fmt.channels = info.channels;
@@ -174,10 +196,23 @@ private:
         dsound_.set_cooperative_level(handle_, ds::priority_level);
         dsound_.format(fmt);
 
-        bgm_.open(
-            io_ex::repeat(vf, -1),
-            audio::widen<float>(dsound_.create_buffer(fmt))
-        );
+        if (offset == -1)
+        {
+            bgm_.open(
+                io_ex::repeat(vf_, -1),
+                audio::widen<float>(dsound_.create_buffer(fmt))
+            );
+        }
+        else
+        {
+            using namespace io_ex::cat_operators;
+
+            bgm_.open(
+                io_ex::lazy_restrict(vf_, 0, offset+len) +
+                io_ex::lazy_restrict(vf_, offset, len) * -1,
+                audio::widen<float>(dsound_.create_buffer(fmt))
+            );
+        }
         bgm_.play();
     }
 
