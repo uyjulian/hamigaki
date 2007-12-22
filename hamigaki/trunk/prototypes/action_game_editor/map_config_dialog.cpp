da502191b0985b7751c9545986c10253e35ceacb
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2007-12-22 13:35:47 +0000

    prepared to add "New" command for MapSelectWindow
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1320 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ hamigaki/trunk/prototypes/action_game_editor/map_config_dialog.cpp
@@ -0,0 +1,94 @@
+// new_stage_dialog.cpp: the dialog to input data for creating a new stage
+
+// Copyright Takeshi Mouri 2007.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// See http://hamigaki.sourceforge.jp/ for library home page.
+
+#include "new_stage_dialog.hpp"
+#include <exception>
+#include "new_dialog.h"
+
+namespace
+{
+
+inline void set_dialog_item_int(::HWND hwnd, int id, int value)
+{
+    ::SetDlgItemInt(hwnd, id, static_cast<unsigned>(value), FALSE);
+}
+
+inline bool get_dialog_item_int(::HWND hwnd, int id, int& value)
+{
+    ::BOOL res;
+    value = static_cast<int>(::GetDlgItemInt(hwnd, id, &res, FALSE));
+    return res != FALSE;
+}
+
+::INT_PTR CALLBACK new_dialog_proc(
+    ::HWND hwndDlg, ::UINT uMsg, ::WPARAM wParam, ::LPARAM lParam)
+{
+    try
+    {
+        if (uMsg == WM_INITDIALOG)
+        {
+            stage_info* info = reinterpret_cast<stage_info*>(lParam);
+            if (info)
+            {
+                ::SetWindowLongPtr(hwndDlg, DWLP_USER, lParam);
+                set_dialog_item_int(hwndDlg, HAMIGAKI_IDC_WIDTH, info->width);
+                set_dialog_item_int(hwndDlg, HAMIGAKI_IDC_HEIGHT, info->height);
+            }
+            return 1;
+        }
+        else if (uMsg == WM_COMMAND)
+        {
+            ::WORD id = LOWORD(wParam);
+            if (id == IDOK)
+            {
+                stage_info* info =
+                    reinterpret_cast<stage_info*>(
+                        ::GetWindowLongPtr(hwndDlg, DWLP_USER)
+                    );
+
+                if (get_dialog_item_int(
+                        hwndDlg, HAMIGAKI_IDC_WIDTH, info->width) &&
+                    get_dialog_item_int(
+                        hwndDlg, HAMIGAKI_IDC_HEIGHT, info->height) )
+                {
+                    ::EndDialog(hwndDlg, IDOK);
+                }
+                return 1;
+            }
+            else if (id == IDCANCEL)
+            {
+                ::EndDialog(hwndDlg, IDCANCEL);
+                return 1;
+            }
+        }
+        else
+            return 0;
+    }
+    catch (const std::exception& e)
+    {
+        ::MessageBoxA(hwndDlg, e.what(), "Action Game Editor", MB_OK);
+    }
+    return 0;
+}
+
+} // namespace
+
+bool get_new_stage_info(::HWND hwnd, stage_info& info)
+{
+    // FIXME
+    ::HINSTANCE module =
+        reinterpret_cast< ::HINSTANCE>(::GetModuleHandle(0));
+
+    ::INT_PTR res = ::DialogBoxParamA(
+        module, MAKEINTRESOURCE(HAMIGAKI_IDD_NEW),
+        hwnd, &new_dialog_proc, reinterpret_cast< ::LPARAM>(&info)
+    );
+
+    return res == IDOK;
+}
