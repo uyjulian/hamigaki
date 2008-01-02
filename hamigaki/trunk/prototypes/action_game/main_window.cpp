54c85b881d7512f55a16afb4025500dfeeeb6082
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-01-02 12:44:10 +0000

    Licence update
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1346 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/prototypes/action_game/main_window.cpp
+++ hamigaki/trunk/prototypes/action_game/main_window.cpp
@@ -1,6 +1,6 @@
 // main_window_class.cpp: main window for action_game
 
-// Copyright Takeshi Mouri 2007.
+// Copyright Takeshi Mouri 2007, 2008.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
