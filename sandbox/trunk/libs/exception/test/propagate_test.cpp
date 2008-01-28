4aa3cdd878d8489b290776919c8a888ef9273576
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-01-28 12:38:53 +0000

    supported VC++
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1426 c04fc264-1e22-0410-9499-a186082fc69e

--- sandbox/trunk/libs/exception/test/propagate_test.cpp
+++ sandbox/trunk/libs/exception/test/propagate_test.cpp
@@ -7,6 +7,7 @@
 
 // See http://hamigaki.sourceforge.jp/ for library home page.
 
+#if defined(__GNUC__)
 #include <cstddef>
 #include <cstring>
 #include <exception>
@@ -111,8 +112,8 @@ public:
     }
 
 private:
-    boost::thread t_;
     my_cxa_exception* e_;
+    boost::thread t_;
 
     void work()
     {
@@ -126,6 +127,170 @@ private:
         }
     }
 };
+#elif defined(_MSC_VER)
+#include <cstring>
+#include <malloc.h>
+#include <windows.h>
+
+extern "C"
+{
+
+struct _tiddata;
+_tiddata* __cdecl _getptd();
+
+} // extern "C"
+
+const ::DWORD cxx_exception_code = 0xE06D7363;
+
+const ::EXCEPTION_RECORD* get_current_exception_record()
+{
+    _tiddata* p = _getptd();
+#if _MSC_VER < 1400
+    return reinterpret_cast< ::EXCEPTION_RECORD**>(p)[31];
+#else
+    return reinterpret_cast< ::EXCEPTION_RECORD**>(p)[34];
+#endif
+}
+
+inline void call_pmfn0(_PMFN f, void* p)
+{
+    __asm
+    {
+        mov ecx, p
+        call f
+    }
+}
+
+inline void call_pmfn1(_PMFN f, void* p0, void* p1)
+{
+    __asm
+    {
+        mov ecx, p0
+        push p1
+        call f
+    }
+}
+
+inline void call_pmfn2(_PMFN f, void* p0, void* p1, int p2)
+{
+    __asm
+    {
+        mov ecx, p0
+        push p2
+        push p1
+        call f
+    }
+}
+
+__declspec(noreturn)
+void rethrow_exception(void* data, const _s__ThrowInfo* info)
+{
+    const _s__CatchableType* t =
+        info->pCatchableTypeArray->arrayOfCatchableTypes[0];
+    int sz = t->sizeOrOffset;
+    _PMFN cp = t->copyFunction;
+    bool is_virtual = (t->properties & 0x04) != 0;
+    void* e = _alloca(sz);
+
+    if (cp)
+    {
+        if (is_virtual)
+            call_pmfn2(cp, e, data, 1);
+        else
+            call_pmfn1(cp, e, data);
+    }
+    else
+        std::memcpy(e, data, sz);
+
+    _CxxThrowException(e, info);
+
+    throw;
+}
+
+
+#include <boost/bind.hpp>
+#include <boost/scoped_array.hpp>
+
+#pragma warning(push)
+#pragma warning(disable : 4251)
+#include <boost/thread.hpp>
+#pragma warning(pop)
+
+class my_thread
+{
+public:
+#pragma warning(push)
+#pragma warning(disable : 4355)
+    my_thread() : t_(boost::bind(&my_thread::work, this))
+    {
+    }
+#pragma warning(pop)
+
+    ~my_thread()
+    {
+        if (e_.get())
+        {
+            if (_PMFN d = ti_->pmfnUnwind)
+                call_pmfn0(d, e_.get());
+        }
+    }
+
+    void wait()
+    {
+        t_.join();
+        if (e_.get())
+            rethrow_exception(e_.get(), ti_);
+    }
+
+private:
+    boost::scoped_array<char> e_;
+    _s__ThrowInfo* ti_;
+    boost::thread t_;
+
+    void work()
+    {
+        try
+        {
+            throw std::runtime_error("error!");
+        }
+        catch (...)
+        {
+            const ::EXCEPTION_RECORD* er = get_current_exception_record();
+            if (er)
+            {
+                if (er->ExceptionCode == cxx_exception_code)
+                {
+                    void* data =
+                        reinterpret_cast<void*>(er->ExceptionInformation[1]);
+
+                    ti_ =
+                        reinterpret_cast<_s__ThrowInfo*>(
+                            er->ExceptionInformation[2]
+                        );
+
+                    const _s__CatchableType* t =
+                        ti_->pCatchableTypeArray->arrayOfCatchableTypes[0];
+
+                    boost::scoped_array<char> e(new char[t->sizeOrOffset]);
+                    if (t->copyFunction)
+                    {
+                        if ((t->properties & 0x04) != 0)
+                            call_pmfn2(t->copyFunction, e.get(), data, 1);
+                        else
+                            call_pmfn1(t->copyFunction, e.get(), data);
+                    }
+                    else
+                        std::memcpy(e.get(), data, t->sizeOrOffset);
+
+                    e_.swap(e);
+                }
+            }
+        }
+    }
+};
+#else
+    #error "Sorry, unsupported compiler"
+#endif
 
 int main()
 {
