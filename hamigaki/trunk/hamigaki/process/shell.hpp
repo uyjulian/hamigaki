c6969f2598cb7b9c8939d462f217a470d62ad329
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-02-09 06:34:14 +0000

    #include BOOST_ABI_PREFIX
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1445 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/process/shell.hpp
+++ hamigaki/trunk/hamigaki/process/shell.hpp
@@ -1,6 +1,6 @@
 // shell.hpp: utilities for command shell
 
-// Copyright Takeshi Mouri 2007.
+// Copyright Takeshi Mouri 2007, 2008.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -11,6 +11,10 @@
 #define HAMIGAKI_PROCESS_LAUNCH_SHELL_HPP
 
 #include <hamigaki/process/child.hpp>
+
+#ifdef BOOST_HAS_ABI_HEADERS
+    #include BOOST_ABI_PREFIX
+#endif
 
 namespace hamigaki { namespace process {
 
@@ -25,5 +29,9 @@ inline child launch_shell(const std::string& cmd)
 HAMIGAKI_PROCESS_DECL std::string shell_expand(const std::string& cmd);
 
 } } // End namespaces process, hamigaki.
+
+#ifdef BOOST_HAS_ABI_HEADERS
+    #include BOOST_ABI_SUFFIX
+#endif
 
 #endif // HAMIGAKI_PROCESS_LAUNCH_SHELL_HPP
