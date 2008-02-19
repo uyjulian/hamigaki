1d3155e9bcb79a015a0fa6a08402d11f166c1aaf
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-02-19 14:50:14 +0000

    added the sections of "project-settings" and "installing-targets"
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1464 c04fc264-1e22-0410-9499-a186082fc69e

---
+++ sandbox/trunk/more/bbv2-tutorial/projects/06/exe/main.cpp
@@ -0,0 +1,13 @@
+// main.cpp: Hello driver program
+
+// Copyright Takeshi Mouri 2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include "hello.hpp"
+
+int main()
+{
+    hello();
+}
