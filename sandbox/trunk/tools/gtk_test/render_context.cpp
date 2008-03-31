0924fbeae15b7de8968ce83eed57b57bb4972fcc
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-03-31 14:22:43 +0000

    changed the pixel format to RGBA for Win32
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1532 c04fc264-1e22-0410-9499-a186082fc69e

--- sandbox/trunk/tools/gtk_test/render_context.cpp
+++ sandbox/trunk/tools/gtk_test/render_context.cpp
@@ -71,7 +71,8 @@ public:
         pfd.nVersion = 1;
         pfd.dwFlags = PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_DOUBLEBUFFER;
         pfd.iPixelType = PFD_TYPE_RGBA;
-        pfd.cColorBits = 24;
+        pfd.cColorBits = 32;
+        pfd.cAlphaBits = 8;
         pfd.cDepthBits = 16;
         pfd.iLayerType = PFD_MAIN_PLANE;
 
