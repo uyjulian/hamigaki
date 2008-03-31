2e79c36ffa5e1ef426a835c919b6dcc7be13e824
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-03-31 12:20:19 +0000

    bumped up the version
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1530 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/version.hpp
+++ hamigaki/trunk/hamigaki/version.hpp
@@ -1,6 +1,6 @@
 // version.hpp: version for Hamigaki Library
 
-// Copyright Takeshi Mouri 2006, 2007.
+// Copyright Takeshi Mouri 2006-2008.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -8,7 +8,7 @@
 #ifndef HAMIGAKI_VERSION_HPP
 #define HAMIGAKI_VERSION_HPP
 
-#define HAMIGAKI_VERSION 4100
-#define HAMIGAKI_LIB_VERSION "0_41"
+#define HAMIGAKI_VERSION 4200
+#define HAMIGAKI_LIB_VERSION "0_42"
 
 #endif // HAMIGAKI_VERSION_HPP
