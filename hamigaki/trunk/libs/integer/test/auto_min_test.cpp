d170ab8e4231c5d94983fcce2678dd2502105285
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2008-05-16 12:15:13 +0000

    removed "using" directives
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1674 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/libs/integer/test/auto_min_test.cpp
+++ hamigaki/trunk/libs/integer/test/auto_min_test.cpp
@@ -1,6 +1,6 @@
 // auto_min_test.cpp: test case for auto_min
 
-// Copyright Takeshi Mouri 2006, 2007.
+// Copyright Takeshi Mouri 2006-2008.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -9,10 +9,11 @@
 
 #include <hamigaki/integer/auto_min.hpp>
 #include <boost/integer/static_min_max.hpp>
+#include <boost/mpl/assert.hpp>
+#include <boost/mpl/bool.hpp>
 #include <boost/test/unit_test.hpp>
 #include <boost/cstdint.hpp>
 #include <boost/integer_traits.hpp>
-#include <boost/static_assert.hpp>
 
 namespace ut = boost::unit_test;
 using hamigaki::detail::auto_min_result;
@@ -50,73 +51,74 @@ struct auto_min_range_check
         (r_min <= t_min) &&
         (r_min <= u_min) &&
         (t_max < u_max ? t_max <= r_max : u_max <= r_max);
+
+    typedef boost::mpl::bool_<value> type;
 };
 
 void auto_min_result_test()
 {
     using namespace boost;
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int8_t  ,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int8_t  ,boost::int64_t >));
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint8_t ,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint8_t ,boost::int64_t >));
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int16_t ,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int16_t ,boost::int64_t >));
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint16_t,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint16_t,boost::int64_t >));
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int32_t ,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int32_t ,boost::int64_t >));
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<uint32_t,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::uint32_t,boost::int64_t >));
 
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,int8_t  >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,uint8_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,int16_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,uint16_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,int32_t >::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,uint32_t>::value ));
-    BOOST_STATIC_ASSERT(( auto_min_range_check<int64_t ,int64_t >::value ));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::int8_t  >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::uint8_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::int16_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::uint16_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::int32_t >));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::uint32_t>));
+    BOOST_MPL_ASSERT((auto_min_range_check<boost::int64_t ,boost::int64_t >));
 }
 
 void auto_min_test()
 {
     using hamigaki::auto_min;
-    using namespace boost;
 
     BOOST_CHECK_EQUAL(auto_min(0,0), 0);
     BOOST_CHECK_EQUAL(auto_min(1,2), 1);
@@ -131,24 +133,24 @@ void auto_min_test()
 
     BOOST_CHECK_EQUAL(
         auto_min(
-            integer_traits<long long>::const_max,
-            integer_traits<unsigned long long>::const_max
+            boost::integer_traits<long long>::const_max,
+            boost::integer_traits<unsigned long long>::const_max
         ),
-        integer_traits<long long>::const_max
+        boost::integer_traits<long long>::const_max
     );
 
     BOOST_CHECK_EQUAL(
         auto_min(
-            integer_traits<long long>::const_min,
-            integer_traits<unsigned long long>::const_max
+            boost::integer_traits<long long>::const_min,
+            boost::integer_traits<unsigned long long>::const_max
         ),
-        integer_traits<long long>::const_min
+        boost::integer_traits<long long>::const_min
     );
 
     BOOST_CHECK_EQUAL(
         auto_min(
-            integer_traits<long long>::const_max,
-            integer_traits<unsigned long long>::const_min
+            boost::integer_traits<long long>::const_max,
+            boost::integer_traits<unsigned long long>::const_min
         ),
         static_cast<long long>(0)
     );
