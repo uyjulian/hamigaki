91902170cf54929a8512b11890c17eae2aaa85b0
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2009-11-14 08:16:44 +0000

    supported VC++9.0/x64
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1752 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/coroutine/detail/fiber_context.hpp
+++ hamigaki/trunk/hamigaki/coroutine/detail/fiber_context.hpp
@@ -1,6 +1,6 @@
 // fiber_context.hpp: Win32 fiber based context implementation
 
-// Copyright Takeshi Mouri 2006-2008.
+// Copyright Takeshi Mouri 2006-2009.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -108,7 +108,8 @@ public:
     {
         if (stack_size == -1)
             stack_size = 0;
-        context_ = ::CreateFiber(stack_size, &trampoline<Functor>, &f);
+        context_ = ::CreateFiber(
+            static_cast<unsigned long>(stack_size), &trampoline<Functor>, &f);
         BOOST_ASSERT(context_ != 0);
     }
 
--- hamigaki/trunk/hamigaki/coroutine/detail/windows/fiber.hpp
+++ hamigaki/trunk/hamigaki/coroutine/detail/windows/fiber.hpp
@@ -1,6 +1,6 @@
 // fiber.hpp: Win32 fiber API declaration
 
-// Copyright Takeshi Mouri 2006, 2007.
+// Copyright Takeshi Mouri 2006-2009.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -12,10 +12,16 @@
 
 #include <boost/config.hpp>
 
-#if defined(_MSC_FULL_VER) && (_MSC_FULL_VER >= 13012035)
+#if defined(_MSC_FULL_VER) && (_MSC_FULL_VER >= 13012035) && defined(_M_IX86)
     #define HAMIGAKI_COROUTINE_HAS_READFSDWORD
     extern "C" unsigned long __readfsdword(unsigned long);
     #pragma intrinsic(__readfsdword)
+#endif
+
+#if defined(_MSC_VER) && defined(_M_AMD64)
+    #define HAMIGAKI_COROUTINE_HAS_READGSQWORD
+    extern "C" unsigned long long __readgsqword(unsigned long);
+    #pragma intrinsic(__readgsqword)
 #endif
 
 #if defined(BOOST_HAS_DECLSPEC) && !defined(HAMIGAKI_COROUTINE_NO_DLLIMPORT)
@@ -28,8 +34,13 @@ extern "C" {
 
 typedef void (__stdcall* start_routine)(void*);
 
+#if !defined(_WIN64)
 HAMIGAKI_COROUTINE_DLLIMPORT
 void* __stdcall CreateFiber(unsigned long, start_routine, void*);
+#else
+HAMIGAKI_COROUTINE_DLLIMPORT
+void* __stdcall CreateFiber(unsigned long long, start_routine, void*);
+#endif
 
 HAMIGAKI_COROUTINE_DLLIMPORT void __stdcall DeleteFiber(void*);
 HAMIGAKI_COROUTINE_DLLIMPORT void* __stdcall ConvertThreadToFiber(void*);
@@ -47,6 +58,13 @@ inline void* get_current_fiber()
 {
     return reinterpret_cast<void*>(
         static_cast< ::uintptr_t>(::__readfsdword(0x10))
+    );
+}
+#elif defined(HAMIGAKI_COROUTINE_HAS_READGSQWORD)
+inline void* get_current_fiber()
+{
+    return reinterpret_cast<void*>(
+        static_cast< ::uintptr_t>(::__readgsqword(0x20))
     );
 }
 #elif (defined(_MSC_VER) || defined(__MWERKS__)) && defined(_M_IX86)
