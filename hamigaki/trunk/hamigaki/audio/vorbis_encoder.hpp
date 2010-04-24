0b7e203a8bd5e66918c4b3c91581788c049a47c9
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2010-04-24 06:20:38 +0000

    workaround for VC10
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1773 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/hamigaki/audio/vorbis_encoder.hpp
+++ hamigaki/trunk/hamigaki/audio/vorbis_encoder.hpp
@@ -1,6 +1,6 @@
 // vorbis_encoder.hpp: vorbisenc device adaptor
 
-// Copyright Takeshi Mouri 2006-2008.
+// Copyright Takeshi Mouri 2006-2010.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -79,7 +79,7 @@ private:
 
     std::streamsize write_blocks(const float* s, std::streamsize n);
 
-#if BOOST_WORKAROUND(_MSC_VER, <= 1500)
+#if BOOST_WORKAROUND(_MSC_VER, <= 1600)
     std::streamsize read_blocks(float* s, std::streamsize n)
     {
         BOOST_ASSERT(false);
