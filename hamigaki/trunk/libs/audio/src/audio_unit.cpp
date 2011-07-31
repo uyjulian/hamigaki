7d8d6e37dedd7def6edf516e7f541b1451e9ce34
hamigaki <hamigaki@c04fc264-1e22-0410-9499-a186082fc69e>
2011-07-31 10:10:16 +0000

    supported Mac OS X 10.7
    git-svn-id: http://svn.osdn.net/svnroot/hamigaki@1781 c04fc264-1e22-0410-9499-a186082fc69e

--- hamigaki/trunk/libs/audio/src/audio_unit.cpp
+++ hamigaki/trunk/libs/audio/src/audio_unit.cpp
@@ -1,6 +1,6 @@
 // audio_unit.cpp: Mac OS X Audio Unit device
 
-// Copyright Takeshi Mouri 2006-2008.
+// Copyright Takeshi Mouri 2006-2011.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -19,6 +19,7 @@
 #include "detail/circular_buffer.hpp"
 #include <AudioToolbox/AudioConverter.h>
 #include <AudioUnit/AudioUnit.h>
+#include <CoreAudio/AudioHardware.h>
 #include <CoreServices/CoreServices.h>
 #include <pthread.h>
 
